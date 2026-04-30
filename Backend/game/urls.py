61750937b4dd9f1f7f6633787f6056634f253de4
jotaFil98 <juan.andresfilgueira@gmail.com>
2026-04-30 19:22:35 -0400

    Cambio en el .api del urls.py

--- Backend/game/urls.py
+++ Backend/game/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import obtener_cartas_aleatorias, listar_cartas
 
 urlpatterns = [
-    # Esta es tu ruta para cartas aleatorias
+    # Quitamos el 'api/' de aquí porque ya está en el core
     path('cartas/random/', obtener_cartas_aleatorias, name='cartas_random'),
-    
-    # Esta es tu ruta para listar todas las cartas
-    path('api/cartas/', listar_cartas, name='listar_cartas'),
+    path('cartas/', listar_cartas, name='listar_cartas'),
 ]
